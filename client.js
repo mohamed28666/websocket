60d202cc59eafce48621d1c6cb322c2e962f2499
mohamed belkahla <mohamed.belkahla19@gmail.com>
2021-04-28 16:01:31 +0200

    first commit

--- client.js
+++ client.js
@@ -1,15 +1,11 @@
 console.log('WebSocket client script will run here.');
-const ws = new WebSocket('ws://localhost:3333');
-// Add a listener that will be triggered when the WebSocket is ready to use
+const ws = new WebSocket('ws://localhost:3210', ['json', 'xml']);
 ws.addEventListener('open', () => {
-  const message = 'Hello!';
-  console.log('Sending:', message);
-  // Send the message to the WebSocket server
-  ws.send(message);
+  const data = { message: 'Hello from the client!' }
+  const json = JSON.stringify(data);
+  ws.send(json);
 });
-// Add a listener in order to process WebSocket messages received from the server
 ws.addEventListener('message', event => {
-  // The `event` object is a typical DOM event object, and the message data sent
-  // by the server is stored in the `data` property
-  console.log('Received:', event.data);
+  const data = JSON.parse(event.data);
+  console.log(data);
 });
